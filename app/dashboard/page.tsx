a67290e7d132a0a754f95439543934ca963db10e
Jian Wei <itsjianweii@gmail.com>
2026-03-04 04:15:32 +0800

    refactor: remove unused imports

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,15 @@
 "use client";
 
-import { DatePickerInput } from "@mantine/dates";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 import { useState } from "react";
 import { useAuthStore } from "@/stores/useAuthStore";
 import { redirect } from "next/navigation";
-import SummaryCard from "@/components/dashboard/overview/SummaryCard";
-import AppointmentList from "@/components/dashboard/overview/AppointmentList";
-import { IconCalendar } from "@tabler/icons-react";
-import { cardBorder } from "@/constants/ui";
-import SummaryChart from "@/components/dashboard/overview/SummaryChart";
+
 import OwnerDashboard from "@/components/ui/dashboard/OwnerDashboard";
 import DoctorDashboard from "@/components/ui/dashboard/DoctorDashboard";
 import StaffDashboard from "@/components/ui/dashboard/StaffDashboard";
-import LocationSelection from "@/components/dashboard/overview/Selection";
+
 import HelperSelection from "@/components/dashboard/overview/features/HelperSelection";
 
 const roleDashboard = {
