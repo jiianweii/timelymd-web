1b59b7ffb28a83092837620d7ef45aa54d264b68
Jian Wei <itsjianweii@gmail.com>
2026-03-20 23:44:03 +0800

    feat: change summarylist type

--- components/dashboard/overview/doctor/SummaryList.tsx
+++ components/dashboard/overview/doctor/SummaryList.tsx
@@ -1,8 +1,11 @@
 import {
   IconCalendar,
-  IconMoneybag,
+  IconCalendarClock,
+  IconClipboardPlus,
   IconMoodHappy,
   IconUser,
+  IconUsers,
+  IconVideo,
 } from "@tabler/icons-react";
 import SummaryCard from "../SummaryCard";
 
@@ -11,31 +14,31 @@ export default function SummaryList() {
     <>
       <SummaryCard
         box="#b7f1ed"
-        icon={IconMoneybag}
-        title="Earnings"
-        value="$3,854"
-        href="/dashboard/revenue"
+        icon={IconCalendar}
+        title="Appointments"
+        value="18"
+        href="/dashboard/doctor/appointments"
       />
       <SummaryCard
         box="#c7f1b7"
-        icon={IconUser}
-        title="Patients"
-        value={"38"}
-        href="/dashboard/patients"
+        icon={IconVideo}
+        title="E-Visit"
+        value={"5"}
+        href="/dashboard/doctor/e-visits"
       />
       <SummaryCard
         box="#f1dfb7"
-        icon={IconCalendar}
-        title="Appointments"
+        icon={IconUsers}
+        title="Queue Length"
         value={"7"}
-        href="/dashboard/outlets"
+        href="/dashboard/doctor/queue"
       />
       <SummaryCard
         box="#b7bdf1"
-        icon={IconMoodHappy}
-        title="Reviews"
-        value={"13"}
-        href="/dashboard/request"
+        icon={IconClipboardPlus}
+        title="Lab Reports"
+        value={"3"}
+        href="/dashboard/doctor/lab"
       />
     </>
   );
