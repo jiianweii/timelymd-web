7f715a884b870011c2d2ffbfbe1a2be339a5dac8
Jian Wei <itsjianweii@gmail.com>
2026-03-20 23:45:35 +0800

    feat: change title to name instead

--- components/dashboard/header/Header.tsx
+++ components/dashboard/header/Header.tsx
@@ -1,20 +1,13 @@
 "use client";
 
-import { capitalizeFirstLetter } from "@/utils/helper";
-
-import { usePathname } from "next/navigation";
 import TeamMembers from "./TeamMembers";
 import Notification from "./Notification";
 import Message from "./Message";
 import Profile from "./Profile";
+import { useAuthStore } from "@/stores/useAuthStore";
 
 export default function Header() {
-  const pathname = usePathname();
-
-  const segments = pathname.split("/").filter(Boolean);
-  const title = segments.length
-    ? capitalizeFirstLetter(segments[segments.length - 1])
-    : capitalizeFirstLetter(pathname);
+  const user = useAuthStore((state) => state.user);
 
   const today = new Date().toLocaleDateString("en-US", {
     weekday: "long",
@@ -26,7 +19,7 @@ export default function Header() {
   return (
     <div className="flex justify-between w-full px-4 py-3 items-center border-b-border border-b">
       <div className="leading-5">
-        <h1 className="font-bold">{title}</h1>
+        <h1 className="font-bold">Hello, {user?.name}</h1>
         <p className="text-xs text-gray-600">{today}</p>
       </div>
       <div className="flex gap-x-4 items-center">
