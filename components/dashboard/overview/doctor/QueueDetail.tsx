f9c21bc5b4e8f5719d8d1fbcfeeda0d1bbe10c40
Jian Wei <itsjianweii@gmail.com>
2026-03-20 23:44:32 +0800

    feat: setup queue table details

--- components/dashboard/overview/doctor/QueueDetail.tsx
+++ components/dashboard/overview/doctor/QueueDetail.tsx
@@ -1,28 +1,62 @@
-import { Button, Table } from "@mantine/core";
+import { Avatar, Button, Table } from "@mantine/core";
 import CurrentQueue from "./CurrentQueue";
 import CustomCard from "@/components/common/CustomCard";
 
 const elements = [
-  { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
-  { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
-  { position: 39, mass: 88.906, symbol: "Y", name: "Yttrium" },
-  { position: 56, mass: 137.33, symbol: "Ba", name: "Barium" },
-  { position: 58, mass: 140.12, symbol: "Ce", name: "Cerium" },
+  {
+    queue: 19,
+    name: "Morgan",
+    type: "Sore Throat",
+    remarks: "Been coughing for weeks",
+  },
+  {
+    queue: 20,
+    name: "Wayne",
+    type: "Fever",
+    remarks: "Head hurts badly and want to faint",
+  },
+  {
+    queue: 21,
+    name: "Josefin",
+    type: "Sprain",
+    remarks: "Leg hurts, cant walk",
+  },
+  {
+    queue: 22,
+    name: "Zeon",
+    type: "Backache",
+    remarks: "Been aching for weeks",
+  },
+  {
+    queue: 23,
+    name: "Raphael",
+    type: "Internal bleeding",
+    remarks: "I pooped too hard that my intestine fell out",
+  },
 ];
 
 export default function QueueDetail() {
   const rows = elements.map((element) => (
     <Table.Tr key={element.name}>
-      <Table.Td>{element.position}</Table.Td>
+      <Table.Td>{element.queue}</Table.Td>
       <Table.Td>{element.name}</Table.Td>
-      <Table.Td>{element.symbol}</Table.Td>
-      <Table.Td>{element.mass}</Table.Td>
+      <Table.Td>{element.type}</Table.Td>
+      <Table.Td>{element.remarks}</Table.Td>
     </Table.Tr>
   ));
   return (
     <CustomCard className="col-span-4 row-span-5 px-3 py-4 flex flex-col shadow-2xs gap-y-3">
       <div className="flex justify-between">
-        <h1 className="font-semibold">Queue Details</h1>
+        <div className="flex gap-x-6 items-center">
+          <div className="flex items-center gap-x-4">
+            <h1 className="font-semibold">Current Queue: </h1>
+            <Avatar color="initials" size={30}>
+              18
+            </Avatar>
+          </div>
+          <p>Progress: 10:02</p>
+        </div>
+
         <Button>Next Queue</Button>
       </div>
       {/* Current Queue Details */}
